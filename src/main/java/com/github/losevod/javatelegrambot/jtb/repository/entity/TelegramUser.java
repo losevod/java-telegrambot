df42f8dd3e727b8ee0f06deb1bbcbe71a57b8a32
LOsevOD <lazar.aleks21@yandex.ru>
2023-03-13 01:09:03 +0300

    JTB-7: added the ability to delete group subscription.

--- src/main/java/com/github/losevod/javatelegrambot/jtb/repository/entity/TelegramUser.java
+++ src/main/java/com/github/losevod/javatelegrambot/jtb/repository/entity/TelegramUser.java
@@ -1,6 +1,7 @@
 package com.github.losevod.javatelegrambot.jtb.repository.entity;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 import java.util.List;
@@ -8,6 +9,7 @@ import java.util.List;
 @Data
 @Entity
 @Table(name = "tg_user")
+@EqualsAndHashCode(exclude = "groupSubs")
 public class TelegramUser {
 
     @Id
---
+++ src/test/java/com/github/losevod/javatelegrambot/jtb/command/DeleteGroupSubCommandTest.java
@@ -0,0 +1,145 @@
+package com.github.losevod.javatelegrambot.jtb.command;
+
+import com.github.losevod.javatelegrambot.jtb.repository.entity.GroupSub;
+import com.github.losevod.javatelegrambot.jtb.repository.entity.TelegramUser;
+import com.github.losevod.javatelegrambot.jtb.service.GroupSubService;
+import com.github.losevod.javatelegrambot.jtb.service.SendBotMessageService;
+import com.github.losevod.javatelegrambot.jtb.service.TelegramUserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import static com.github.losevod.javatelegrambot.jtb.command.AbstractCommandTest.prepareUpdate;
+import static com.github.losevod.javatelegrambot.jtb.command.CommandName.DELETE_GROUP_SUB;
+import static java.util.Collections.singletonList;
+
+@DisplayName("Unit-level testing for DeleteGroupSubCommand")
+public class DeleteGroupSubCommandTest {
+
+    private Command command;
+    private SendBotMessageService sendBotMessageService;
+    GroupSubService groupSubService;
+    TelegramUserService telegramUserService;
+
+    @BeforeEach
+    public void init() {
+        sendBotMessageService = Mockito.mock(SendBotMessageService.class);
+        groupSubService = Mockito.mock(GroupSubService.class);
+        telegramUserService = Mockito.mock(TelegramUserService.class);
+
+        command = new DeleteGroupSubCommand(sendBotMessageService, groupSubService, telegramUserService);
+    }
+
+    @Test
+    public void shouldProperlyReturnEmptySubscriptionList() {
+        //given
+        Long chatId = 23456L;
+        Update update = prepareUpdate(chatId, DELETE_GROUP_SUB.getCommandName());
+
+        Mockito.when(telegramUserService.findByChatId(String.valueOf(chatId)))
+                .thenReturn(Optional.of(new TelegramUser()));
+
+        String expectedMessage = "Пока нет подписок на группы. Чтобы добавить подписку напиши /addGroupSub";
+
+        //when
+        command.execute(update);
+
+        //then
+        Mockito.verify(sendBotMessageService).sendMessage(chatId.toString(), expectedMessage);
+    }
+
+    @Test
+    public void shouldRejectByInvalidGroupId() {
+        //given
+        Long chatId = 23456L;
+        Update update = prepareUpdate(chatId, String.format("%s %s", DELETE_GROUP_SUB.getCommandName()));
+        TelegramUser telegramUser = new TelegramUser();
+        GroupSub gs1 = new GroupSub();
+        gs1.setId(123);
+        gs1.setTitle("GS1 Title");
+        telegramUser.setGroupSubs(singletonList(gs1));
+        Mockito.when(telegramUserService.findByChatId(String.valueOf(chatId)))
+                .thenReturn(Optional.of(telegramUser));
+
+        String expectedMessage = "неправильный формат ID группы.\n " +
+                "ID должно быть целым положительным числом";
+
+        //when
+        command.execute(update);
+
+        //then
+        Mockito.verify(sendBotMessageService).sendMessage(chatId.toString(), expectedMessage);
+    }
+
+    @Test
+    public void shouldProperlyDeleteByGroupId() {
+        //given
+
+        ///prepare update object
+        Long chatId = 23456L;
+        Integer groupId = 1234;
+        Update update = prepareUpdate(chatId, String.format("%s %s", DELETE_GROUP_SUB.getCommandName(), groupId));
+
+
+        GroupSub gs1 = new GroupSub();
+        gs1.setId(123);
+        gs1.setTitle("GS1 Title");
+        TelegramUser telegramUser = new TelegramUser();
+        telegramUser.setChatId(chatId.toString());
+        telegramUser.setGroupSubs(singletonList(gs1));
+        ArrayList<TelegramUser> users = new ArrayList<>();
+        users.add(telegramUser);
+        gs1.setUsers(users);
+        Mockito.when(groupSubService.findById(groupId)).thenReturn(Optional.of(gs1));
+        Mockito.when(telegramUserService.findByChatId(String.valueOf(chatId)))
+                .thenReturn(Optional.of(telegramUser));
+
+        String expectedMessage = "Удалил подписку на группу: GS1 Title";
+
+        //when
+        command.execute(update);
+
+        //then
+        users.remove(telegramUser);
+        Mockito.verify(groupSubService).save(gs1);
+        Mockito.verify(sendBotMessageService).sendMessage(chatId.toString(), expectedMessage);
+    }
+
+    @Test
+    public void shouldDoesNotExistByGroupId() {
+        //given
+        Long chatId = 23456L;
+        Integer groupId = 1234;
+        Update update = prepareUpdate(chatId, String.format("%s %s", DELETE_GROUP_SUB.getCommandName(), groupId));
+
+
+        Mockito.when(groupSubService.findById(groupId)).thenReturn(Optional.empty());
+
+        String expectedMessage = "Не нашел такой группы =/";
+
+        //when
+        command.execute(update);
+
+        //then
+        Mockito.verify(groupSubService).findById(groupId);
+        Mockito.verify(sendBotMessageService).sendMessage(chatId.toString(), expectedMessage);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
